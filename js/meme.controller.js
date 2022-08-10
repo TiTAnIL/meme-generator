7490cb02340b8467aa09f7f0b950737256b5f6b0
Denis <denismagin@gmail.com>
2022-08-10 11:39:48 +0300

    first commit

---
+++ js/meme.controller.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let gElCanvas
+let gCtx
+
+function init() {
+    gElCanvas = document.querySelector('#canvas')
+    gCtx = gElCanvas.getContext('2d')
+    console.log('ctx', gCtx)
+}
